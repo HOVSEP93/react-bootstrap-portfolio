1fd1b2b21c7c20aec1fdf8fb8e2bf020c8eeef42
HovsepStepanian <hovsepstepanian@gmail.com>
2021-10-23 00:31:20 +0300

    Header commponent completed

---
+++ src/components/header/MainTitle.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react'
+import Vivus from 'vivus'
+
+const MainTitle = () => {
+  useEffect(() => {
+    new Vivus('main-title', {duration: 200, file: 'svg/main-title_animated.svg'})
+  }, [])
+  return (
+    <div id='main-title'></div>
+  )
+}
+
+export default MainTitle
---
+++ src/components/header/index.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "./header.css";
+import { motion } from "framer-motion";
+import Particles from "react-particles-js";
+import author from "./me.jpg";
+import MainTitle from "./MainTitle";
+
+const Header = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <>
+      <Particles
+        style={{ position: "absolute" }}
+        params={{
+          particles: {
+            shape: {
+              type: "images",
+              image: {
+                src: "react-logo-black.svg",
+              },
+            },
+            number: {
+              value: 5,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            opacity: {
+              value: 0.2,
+            },
+            polygon: {
+              nb_sides: 15,
+            },
+            size: {
+              value: 30,
+            },
+            line_linked: {
+              enable: false,
+            },
+          },
+        }}
+      />
+      <div className="header">
+        <img src={author} alt="Andrey Dovganyuk" />
+        <MainTitle />
+        <motion.div
+          className="icons-wrapper"
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+          <div style={{ display: "flex" }}>
+            <motion.div className="item-1 item" variants={item}></motion.div>
+            <motion.div className="item-2 item" variants={item}></motion.div>
+            <motion.div className="item-3 item" variants={item}></motion.div>
+            <motion.div className="item-4 item" variants={item}></motion.div>
+            <motion.div className="item-5 item" variants={item}></motion.div>
+            <motion.div className="item-6 item" variants={item}></motion.div>
+            <motion.div className="item-7 item" variants={item}></motion.div>
+            <motion.div className="item-8 item" variants={item}></motion.div>
+          </div>
+        </motion.div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
